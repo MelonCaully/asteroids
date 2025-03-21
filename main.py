b8dc8b78f54b0d25bd54836d12af66efb144aac3
Meloncaully <maloneingham@gmail.com>
2025-03-21 14:16:29 -0400

    updated main.py with screen frames

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 # this allows us to use the code from the open-source pygame library
-import pygame
-import constants
+import pygame as pg
+from constants import *
 
 def main():
+    pg.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
-    return None
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(000)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        pg.display.flip()
+    
 
 if __name__ == "__main__":
     main()
